a48cd778ff496aa3bc3b96b1f7b31d703e0c5d61
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 16:37:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,10 +3,11 @@ function shout(string) {
 } 
 
 function whisper(string) {
-  return string.toLowerCase()
+  return string.toLowerCase() 
 }
 
 function logShout(string) {
+  console.log(string.toUpperCase()); 
   
 }
 
