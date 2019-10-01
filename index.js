953d34bb7ff161b7b6f8ef4b47a346f71e5e7f2a
remi.odufuye <remi.odufuye@gmail.com>
2019-10-01 17:01:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,9 +21,9 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase() === string ) {
     return `I can't hear you!` ; 
   }
-  
   else if (string.toUpperCase() === string ) {
     return 'YES INDEED!` ; 
   }
   
 }
+
